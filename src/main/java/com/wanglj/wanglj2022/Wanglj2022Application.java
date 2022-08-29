1c68b8eaa4c4489f5e4eee7b1070eb87ccf1365f
Wanglj <wanglj@easier.cn>
2022-08-29 23:00:07 +0800

    wanglj-2022 first commit

---
+++ src/main/java/com/wanglj/wanglj2022/Wanglj2022Application.java
@@ -0,0 +1,13 @@
+package com.wanglj.wanglj2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wanglj2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wanglj2022Application.class, args);
+    }
+
+}
