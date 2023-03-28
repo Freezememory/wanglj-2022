9c0e8a46945731019d766e3273ca4c08e6a3ed9f
Wanglj <1152589335@qq.com>
2023-03-29 00:09:10 +0800

    堆排序

---
+++ src/main/java/com/wanglj/wanglj2022/algorithm/HeapSort.java
@@ -0,0 +1,47 @@
+package com.wanglj.wanglj2022.algorithm;
+
+import java.util.Arrays;
+
+public class HeapSort {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,6,9,10,1,4,3,2};
+        heapSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    private static int heapLen;
+
+    public static void heapSort(int[] arr) {
+        heapLen = arr.length;
+        for (int i = heapLen - 1; i >= 0; i--) {
+            heapify(arr, i);
+        }
+
+        for (int i = heapLen - 1; i > 0; i--) {
+            swap(arr, 0, heapLen - 1);
+            heapLen--;
+            heapify(arr, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int idx) {
+        int left = idx * 2 + 1, right = idx * 2 + 2, largest = idx;
+        if (left < heapLen && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if (right < heapLen && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != idx) {
+            swap(arr, largest, idx);
+            heapify(arr, largest);
+        }
+    }
+
+    private static void swap(int[] arr, int idx1, int idx2) {
+        int tmp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = tmp;
+    }
+}
