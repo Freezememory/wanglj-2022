504dee928f5e33122a8176667c8518a196466aff
Wanglj <1152589335@qq.com>
2023-04-02 14:27:22 +0800

    基数排序

---
+++ src/main/java/com/wanglj/wanglj2022/algorithm/RadixSort.java
@@ -0,0 +1,48 @@
+package com.wanglj.wanglj2022.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//基数排序
+public class RadixSort {
+
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,6,9,10,1,4,3,2};
+        radixSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void radixSort(int[] arr) {
+        if (arr.length < 2) return;
+        int maxVal = arr[0];//求出最大值
+        for (int a : arr) {
+            if (maxVal < a) {
+                maxVal = a;
+            }
+        }
+        int n = 1;
+        while (maxVal / 10 != 0) {//求出最大值位数
+            maxVal /= 10;
+            n++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            List<List<Integer>> radix = new ArrayList<>();
+            for (int j = 0; j < 10; j++) {
+                radix.add(new ArrayList<>());
+            }
+            int index;
+            for (int a : arr) {
+                index = (a / (int) Math.pow(10, i)) % 10;
+                radix.get(index).add(a);
+            }
+            index = 0;
+            for (List<Integer> list : radix) {
+                for (int a : list) {
+                    arr[index++] = a;
+                }
+            }
+        }
+    }
+}
