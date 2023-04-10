b9137473d05f4a624623055e1527f454a1747315
Wanglj <1152589335@qq.com>
2023-04-10 14:29:00 +0800

    单例模式

---
+++ src/main/java/com/wanglj/wanglj2022/study/designPattern/singleton/Singleton.java
@@ -0,0 +1,45 @@
+package com.wanglj.wanglj2022.study.designPattern.singleton;
+
+/**
+ * @author Wanglj
+ * @date 2023/4/10 11:46
+ * @desc SingleTon
+ */
+public class Singleton {
+
+    private Singleton() {}
+
+    /*private static  Singleton instance;
+    static {
+        instance = new Singleton();
+    }*/
+
+   /* private static final Singleton instance = new Singleton();
+      public static Singleton getInstance() {
+        return instance;
+      }*/
+
+
+    /* private static class SingletonInstance{
+        private final static Singleton INSTANCE = new Singleton();
+      }
+
+    public static Singleton getInstance() {
+        return SingletonInstance.INSTANCE;
+    }*/
+
+    private static volatile Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+
+}
